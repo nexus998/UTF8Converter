5b531d8e7a28e90ce7bc4f577832ad94754d79f8
nexus998 <mindaugasm.morkunas@gmail.com>
2019-10-24 12:17:38 +0300

    Added converters
    Convert decimal to unicode code point, UTF-8 bytes in hex, UTF-8 encoded char, convert very big file encoded with CP437 to UTF-8

---
+++ UTF8Converter/DecimalToUnicodeConverter.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UTF8Converter
+{
+    class DecimalToUnicodeConverter
+    {
+        public DecimalToUnicodeConverter()
+        {
+            var f = new ProgramFunction("Convert decimal to UTF-8", Convert);
+        }
+
+        
+
+        void Convert()
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            Encoding utf8 = new UTF8Encoding();
+            
+
+            Console.Clear();
+            Console.WriteLine();
+            Console.WriteLine("Enter a decimal number to convert it to an UTF-8 symbol and show Unicode and UTF-8 number codes.");
+            Console.WriteLine("Enter the command BACK to return to the menu.");
+            while (true)
+            {
+                string val = Console.ReadLine();
+
+                if (val.ToLower() == "back")
+                {
+                    Program.DoWelcomeScreen();
+                    break;
+                }
+
+                int intValue;
+                if (!int.TryParse(val, out intValue))
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine(val + " That is not a decimal number.");
+                    Console.ForegroundColor = ConsoleColor.White;
+                    continue;
+                }
+
+                
+
+                //Display unicode code point
+                Console.WriteLine();
+                Console.WriteLine("Unicode number: U+" + intValue.ToString("X4"));
+
+                //Display UTF-8 bytes in hex
+                Console.Write("UTF-8 number: ");
+                byte[] utf8bytes = GetBytesFromUnicodeCodePoint(intValue);
+                foreach (var byt in utf8bytes)
+                {
+                    Console.Write(byt.ToString("X2") + " ");
+                }
+                //Display character encoded in UTF-8
+                Console.Write("\nCharacter: ");
+                string unicodeString = char.ConvertFromUtf32(intValue).ToString();
+                Console.WriteLine(unicodeString);
+                Console.WriteLine();
+            }
+        }
+        public static byte[] GetBytesFromUnicodeCodePoint(int intValue)
+        {
+            return Encoding.UTF8.GetBytes(char.ConvertFromUtf32(intValue).ToString());
+        }
+    }
+}
--- UTF8Converter/Program.cs
+++ UTF8Converter/Program.cs
@@ -10,6 +10,56 @@ namespace UTF8Converter
     {
         static void Main(string[] args)
         {
+            var d = new DecimalToUnicodeConverter();
+            var c = new ConverterFunction();
+            var e = new ExitFunction();
+            DoWelcomeScreen();
+        }
+        static bool correctSelection = false;
+        public static void DoWelcomeScreen()
+        {
+            correctSelection = false;
+            Console.Clear();
+            Console.WriteLine();
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("Unicode to UTF-8 Converter");
+            Console.WriteLine("(made by Mindaugas Morkunas)");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine();
+
+            LoadPrograms();
+
+            while (!correctSelection)
+            {
+                var input = Console.ReadKey();
+                char res = '0';
+                try
+                {
+                    string inp = input.Key.ToString();
+                    if (inp.StartsWith("NumPad") || inp.StartsWith("D"))
+                    {
+                        res = inp.Last();
+                    }
+                    var selectedFunction = ProgramSettings.GetProgramFunction(int.Parse(res.ToString()) - 1);
+                    selectedFunction.RunProgram();
+                    correctSelection = true;
+                    Console.Clear();
+                }
+                catch
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine(" - Command not found. Press try again...");
+                    Console.ForegroundColor = ConsoleColor.White;
+                }
+            }
+        }
+        static void LoadPrograms()
+        {
+            for (int i = 0; i < ProgramSettings.GetProgramFunctions().Count; i++)
+            {
+                Console.WriteLine((i + 1) + ". " + ProgramSettings.GetProgramFunctions()[i].GetName());
+            }
         }
     }
 }
+
